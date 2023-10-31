8456294f9730024d2765e60461ee632c1ac42569
kenhuey <kenhuey@mythsart.com>
2023-11-01 05:27:34 +0800

    chore:

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import GlobalWindowRouter from "./global-window-router.vue";
 
+// TODO: TSX 全局管理 等等高级一点的技术，不要滥用setup和模板了，太low了
+
+// TODO: 仿造主进程做入口封装
 createApp(GlobalWindowRouter).mount("#window-frame");
